7dbfe2a41c421c58a6f6e11d6e871981bdc2dcd0
EMANOEL VITOR VENTURA ATANAZIO <e.atanazio@labcetec.net>
2024-10-02 17:00:01 -0300

    Criando navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import Home from './components/views/Home.jsx';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './components/views/Login.jsx';
+import GestaoUsuario from './components/views/GestaoUsuario.jsx';
+import ImportCSV from './components/views/ImportCSV.jsx';
+import CadastroAula from './components/views/CadastroAula.jsx';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/gestao_usuario' element={<GestaoUsuario/>}/>
+        <Route path='/import_csv' element={<ImportCSV/>}/>
+        <Route path='/cadastro_aula' element={<CadastroAula/>}/>
       </Routes>
     </Router>
   );
