8f4c51b564637c6cbc72a0e167a52e531c570a7b
EMANOEL VITOR VENTURA ATANAZIO <e.atanazio@labcetec.net>
2024-10-17 17:01:08 -0300

    criando post com o banco

--- server/src/controllers/AulaController.js
+++ server/src/controllers/AulaController.js
@@ -1,12 +1,16 @@
 import { createAula } from "../models/AulaModel.js";
+import { showAulas } from "../models/AulaModel.js";
 
-export async function criarAula(req,res) {
+export async function criarAula(req, res) {
     console.log('AulaController criarAula');
-    
+
     //Criando constante com requisição
     const aula = req.body;
 
-    
+    //Exibindo corpo da requisição
+    console.log(aula);
+
+
     //Tentando criar aula
     try {
         const [status, resposta] = await createAula(aula);
@@ -19,21 +23,14 @@ export async function criarAula(req,res) {
 
 }
 
-export async function mostrarAulas(req,res){
-    return res.status(200).json(
-        [
-            {
-              "id": "1300",
-              "data": "2024-10-09",
-              "data_hora_inicio": "13:30",
-              "data_hora_fim": "17:30",
-              "turma": "EMP-NBM-03",
-              "instrutor": "AMANDA DIAS",
-              "unidade_curricular": "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
-              "ambiente": "VTRIA-3-SALA-3004",
-              "chave": null
-              
-            }
-        ]
-    );
+export async function mostrarAulas(req, res) {
+    const aula = req.body;
+
+    try {
+        const [status, resposta] = await showAulas(aula);
+        res.status(status).json(resposta);
+    } catch (error) {
+        console.log(error);
+        res.status(502).json(error);
+    }
 }
--- server/src/models/AulaModel.js
+++ server/src/models/AulaModel.js
@@ -1,11 +1,12 @@
 import mysql from 'mysql2/promise'
 import db from '../conexao.js'
 
-//Criando conexão para o banco de dados usando configurações de 'db'
-const conexao = mysql.createPool(db);
+
+
 
 export async function createAula(aula){
-    
+    //Criando conexão para o banco de dados usando configurações de 'db'
+    const conexao = mysql.createPool(db);
     const sql = `INSERT INTO aulas( 
     data, 
     data_hora_inicio, 
@@ -30,10 +31,35 @@ export async function createAula(aula){
     
     // Executando query no banco
     try {
-        const [retorno] = await this.conexao.query(sql, params);
-        return [201, retorno];
+        const [retorno] = await conexao.query(sql, params);
+        console.log('Aula cadastrada')
+        return [201, 'Aula cadastrada'];
     } catch (error) {
         console.log(error);
         return [500, error];
+    }
+}
+
+export async function showAulas(aula) {
+    const conexao = mysql.createPool(db);
+    const sql = `SELECT * FROM aulas`;
+
+    const params = [
+        aula.data,
+        aula.data_hora_inicio,
+        aula.data_hora_fim,
+        aula.turma,
+        aula.instrutor,
+        aula.unidade_curricular,
+        aula.ambiente
+    ];
+
+    try {
+        const [retorno] = await conexao.query(sql, params);
+        console.log('Mostrando aulas');
+        return[200, retorno]
+    } catch (error) {
+        console.log(error);
+        return[502, error];
     }
 }
